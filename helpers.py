7da67b8626c91f3e8738de9cdbbd9e952be4593a
othmane <othmanechahid1@gmail.com>
2022-04-13 14:27:15 +0200

    Add files

---
+++ helpers.py
@@ -0,0 +1,6 @@
+def somme(a, b):
+    """
+    >>> somme(2,6)
+    8
+    """
+    return int(a) + int(b)
---
+++ main.py
@@ -0,0 +1,10 @@
+from bottle import route, run
+from helpers import somme
+import sys
+
+
+@route("/add/<a>/<b>")
+def addition_test(a="0", b="0"):
+    return {"result": somme(a, b)}
+
+run(host="0.0.0.0", port=sys.argv[1], reloader=True)
